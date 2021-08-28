5683376f30b04611cab2392993c03ba14f4b8c5d
Florian Bruggisser <florian@bildspur.ch>
2021-08-28 17:22:36 +0200

    implemented basic webcam detection

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# multi-pose-mediapipe
-Combining human detection algorithm with mediapipe pose network.
+# Multi Pose MediaPipe
+[MediaPipe](https://google.github.io/mediapipe/) multi pose detection example.
+
+### Install & Run
+
+Currently, this is only tested on Windows and MacOS. It's recommended to use Python3 (`>3.7`) and a virtual environment.
+
+```bash
+python install -r requirements.txt
+```
+
+To run an example use the basic python command to start up the script.
+
+```bash
+# start pose detection with webcam
+python pose.py
+
+# start pose detection with video
+python pose.py --input yoga.mp4
+```
+
+### About
+Based on [mediapipe-osc](https://github.com/cansik/mediapipe-osc/).
---
+++ pose.py
@@ -0,0 +1,61 @@
+import argparse
+
+import cv2
+import mediapipe as mp
+from mediapipe.framework.formats import landmark_pb2
+
+from utils import add_default_args, get_video_input
+
+
+def main():
+    # read arguments
+    parser = argparse.ArgumentParser()
+    add_default_args(parser)
+    parser.add_argument("--model-complexity", default=1, type=int,
+                        help="Set model complexity (0=Light, 1=Full, 2=Heavy).")
+    parser.add_argument("--no-smooth-landmarks", action="store_false", help="Disable landmark smoothing.")
+    parser.add_argument("--static-image-mode", action="store_true", help="Enables static image mode.")
+    args = parser.parse_args()
+
+    # setup camera loop
+    mp_drawing = mp.solutions.drawing_utils
+    mp_pose = mp.solutions.pose
+
+    pose = mp_pose.Pose(
+        smooth_landmarks=args.no_smooth_landmarks,
+        static_image_mode=args.static_image_mode,
+        model_complexity=args.model_complexity,
+        min_detection_confidence=args.min_detection_confidence,
+        min_tracking_confidence=args.min_tracking_confidence)
+    cap = cv2.VideoCapture(get_video_input(args.input))
+
+    # fix bug which occurs because draw landmarks is not adapted to upper pose
+    connections = mp_pose.POSE_CONNECTIONS
+
+    while cap.isOpened():
+        success, image = cap.read()
+        if not success:
+            break
+
+        # Flip the image horizontally for a later selfie-view display, and convert
+        # the BGR image to RGB.
+        image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+        # To improve performance, optionally mark the image as not writeable to
+        # pass by reference.
+        image.flags.writeable = False
+        results = pose.process(image)
+
+        # Draw the pose annotation on the image.
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, connections)
+        cv2.imshow('MediaPipe Multi Pose', image)
+        if cv2.waitKey(5) & 0xFF == 27:
+            break
+    pose.close()
+    cap.release()
+
+
+if __name__ == "__main__":
+    main()
